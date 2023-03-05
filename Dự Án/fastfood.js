63a3302f57e50d5cbb11901984ae911d091c4af3
HuynhNam0 <121837424+HuynhNam0@users.noreply.github.com>
2023-03-05 20:19:10 +0700

    Create fastfood.js

---
+++ Dự Án/fastfood.js
@@ -0,0 +1,11 @@
+const menuBar = document.querySelector(".menu-bar");
+menuBar.addEventListener("click", function () {
+  menuBar.classList.toggle("active");
+  document.querySelector(".menu-items").classList.toggle("active");
+});
+
+const columsRight = document.querySelector("#menu");
+columsRight.addEventListener("click", function () {
+  columsRight.classList.toggle("active");
+  document.querySelector(".colums").classList.toggle("active");
+});
